756af6d2e18043188efc07e98c5465fbb8afde62
Nicolas Marolleau <nmarolleau@r2.fr>
2026-02-04 17:55:32 +0100

    amelioration js scrollToAnchor

--- _dev/js/core.js
+++ _dev/js/core.js
@@ -27,7 +27,7 @@ import prestashop from 'prestashop';
 import EventEmitter from 'events';
 
 import SlickSlider from './components/slick';
-import  '../node_modules/bootstrap/js/src/collapse.js';
+import '../node_modules/bootstrap/js/src/collapse.js';
 import 'bootstrap/js/src/collapse';
 import './responsive';
 import './cart';
@@ -42,7 +42,7 @@ import lazysizes from 'lazysizes'
 
 
 // "inherit" EventEmitter
-        for (var i in EventEmitter.prototype) {
+for (var i in EventEmitter.prototype) {
     prestashop[i] = EventEmitter.prototype[i];
 }
 
@@ -110,7 +110,7 @@ $(document).ready(function () {
         //$('#adtm_menu .advtm_menu_toggle').toggleClass('adtm_menu_mobile_mode');
     }
 
-    function scrollToAnchor(anchor) {
+    function scrollToAnchorV1(anchor) {
         console.log(anchor);
         var target = $(anchor);
         var scrollTop = target.offset().top - $('#header').height() - 40;
@@ -135,65 +135,97 @@ $(document).ready(function () {
 
     }
 
+    function scrollToAnchor(anchor) {
+
+        var $target = $(anchor);
+        if (!$target.length) return;
+
+        var headerHeight = $('#header').outerHeight() || 0;
+        var targetTop = $target.offset().top - headerHeight - 40;
+        var currentScroll = $(window).scrollTop();
+
+        var distance = Math.abs(currentScroll - targetTop);
+
+        console.log('Current:', currentScroll);
+        console.log('Target:', targetTop);
+        console.log('Distance:', distance);
+
+        // Si déjà proche → ne rien faire
+        if (distance < 5) return;
+
+        // Durée dynamique (ajuste si besoin)
+        var duration = Math.min(1200, Math.max(200, distance * 0.5));
+
+        $('html, body').stop(true).animate({
+            scrollTop: targetTop
+        }, duration, function () {
+
+            // Gestion focus accessibilité
+            $target.attr('tabindex', '-1').focus();
+
+        });
+    }
+
+
     // Fonction pour scroller les liens vers ancres
     $('a[href*="#"]')
-            // Remove links that don't actually link to anything
-            .not('[href="#"]')
-            .not('[href*="#footer"]')
-            .not('.footer__title--mobile')
-            .not('.noscroll')
-            /*
-             .not('[href="#0"]')
-             .not('[href="#bt_tabs-0"]')
-             .not('[href="#bt_tabs-1"]')*/
-            .click(function (event) {
-                // On-page links
-                if (
-                        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                        &&
-                        location.hostname == this.hostname
-                        ) {
-                    // Figure out element to scroll to
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href*="#footer"]')
+        .not('.footer__title--mobile')
+        .not('.noscroll')
+        /*
+         .not('[href="#0"]')
+         .not('[href="#bt_tabs-0"]')
+         .not('[href="#bt_tabs-1"]')*/
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
 
-                    if ($(this).hasClass('nav-link')) {
-                        var target = $(this).parent().parent();
-                    } else {
-                        var target = $(this.hash);
-                        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                    }
-
-                    console.log('target');
-                    console.log(target);
-                    console.log(target.offset().top);
-                    console.log('header height');
-                    console.log($('#header').height());
-
-                    // Does a scroll target exist?
-                    if (target.length) {
-                        // Only prevent default if animation is actually gonna happen
-                        //event.preventDefault();
-
-                        var scrollTop = target.offset().top - $('#header').height() - 40;
-                        console.log('header height: ' + $('#header').height());
-                        console.log('scrollTop : ' + scrollTop);
-
-                        $('html, body').animate({
-                            scrollTop: scrollTop
-                        }, 1000, function () {
-                            // Callback after animation
-                            // Must change focus!
-                            var $target = $(target);
-                            $target.focus();
-                            if ($target.is(":focus")) { // Checking if the target was focused
-                                return false;
-                            } else {
-                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                                $target.focus(); // Set focus again
-                            }
-                        });
-                    }
+                if ($(this).hasClass('nav-link')) {
+                    var target = $(this).parent().parent();
+                } else {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 }
-            });
+
+                console.log('target');
+                console.log(target);
+                console.log(target.offset().top);
+                console.log('header height');
+                console.log($('#header').height());
+
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    //event.preventDefault();
+
+                    var scrollTop = target.offset().top - $('#header').height() - 40;
+                    console.log('header height: ' + $('#header').height());
+                    console.log('scrollTop : ' + scrollTop);
+
+                    $('html, body').animate({
+                        scrollTop: scrollTop
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        }
+                    });
+                }
+            }
+        });
 
 
 });
